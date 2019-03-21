f423fbf68c81bc68b44ad1c5de6ef243c7f7c87e
Clemens Elflein <clemens1@familie-elflein.de>
2019-03-21 13:41:09 +0100

    Added own firmware

---
+++ DefaultFirmware/DefaultFirmware.ino
@@ -0,0 +1,55 @@
+/*
+  Self-o-mat Loader
+
+  This program is the one burned along with the customized bootloader onto each self-o-mat.
+
+  by Clemens Elflein
+
+  https://self-o-mat.de/
+*/
+
+// PIN_ON controls the power of the whole self-o-mat
+const int PIN_ON=8;
+
+// PIN_STATUS controls the status LED on the board.
+const int PIN_STATUS=LED_BUILTIN;
+
+bool waitingForSpace;
+
+void setup() {
+  waitingForSpace = false;
+  
+  delay(1000);
+  pinMode(PIN_STATUS, OUTPUT);
+  pinMode(PIN_ON, OUTPUT);
+
+  // Enable Power
+  digitalWrite(PIN_ON, HIGH);
+
+  // Wait for avrdude commands
+  Serial.begin(38400);
+}
+
+
+void loop() {
+  while(Serial.available()) {
+    char c = Serial.read();
+    switch(c) {
+      case '0':
+        waitingForSpace = true;
+        break;
+      case ' ':
+        if(waitingForSpace) {
+          digitalWrite(PIN_STATUS, HIGH);
+          typedef void (*do_reboot_t)(void);
+          const do_reboot_t do_reboot = (do_reboot_t)((FLASHEND - 511) >> 1);
+          cli(); TCCR0A = TCCR1A = TCCR2A = 0; // make sure interrupts are off and timers are reset.
+          do_reboot();
+        }
+      default:
+        waitingForSpace = false;    
+    }
+  }
+  // Blink the LED to show the user that the bootloader init firmware is running
+  digitalWrite(PIN_STATUS, 1 & (millis() >> 5));
+}
